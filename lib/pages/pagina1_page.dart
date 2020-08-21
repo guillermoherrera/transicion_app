db8d3556ac025ee7507f3776c2daa887da6342dd
Guillermo <guillhermoherrera@gmail.com>
2020-08-21 09:02:22 -0500

    SlideTransition

--- lib/pages/pagina1_page.dart
+++ lib/pages/pagina1_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:trancision_app/pages/pagina2_page.dart';
 
 class Pagina1Page extends StatelessWidget {
   @override
@@ -12,8 +13,29 @@ class Pagina1Page extends StatelessWidget {
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.access_time),
-        onPressed: (){}
+        onPressed: (){
+          Navigator.push(context, _crearRuta());
+          //Navigator.push(context, MaterialPageRoute(builder: (context)=> Pagina2Page()));
+        }
       ),
     );
+  }
+
+  Route _crearRuta(){
+
+    return PageRouteBuilder(
+      pageBuilder: (BuildContext context, Animation<double> animation, Animation<double> secondaryAnimation) => Pagina2Page(),
+      transitionDuration: Duration(seconds: 2),
+      transitionsBuilder: ( context, animation, sencondaryAnimation, child ) {
+
+        final curveAnimation = CurvedAnimation(parent: animation, curve: Curves.easeInOut);
+
+        return SlideTransition(
+          position: Tween<Offset>(begin: Offset(0.5, 1.0), end: Offset.zero).animate(curveAnimation),
+          child: child,
+        );
+      }
+    );
+
   }
 }
